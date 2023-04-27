7e24420586dc9ee911d2aca472ca0d67bea49cae
ethanscm <madjuse@uci.edu>
2023-04-26 21:41:50 -0700

    Updated Word Frequency Count Tracking and Sorting

--- scraper.py
+++ scraper.py
@@ -6,7 +6,59 @@ from SimHash import SimHash
 
 class text_tracker:
     all_words = {}
+    tokens = []
     longest_page = ("", 0)
+
+    def SortFrequencies(array):
+        if(len(array) == 1):
+            return array
+        elif (len(array) == 0):
+            return array
+        
+        mid = len(array) // 2
+        left = text_tracker.SortFrequencies(array[:mid])
+        right = text_tracker.SortFrequencies(array[mid:])
+
+        l = 0
+        r = 0
+        t = []
+
+        while l < len(left) and r < len(right):
+            if text_tracker.all_words[left[l]] > text_tracker.all_words[right[r]]:
+                t.append(left[l])
+                l += 1
+            elif text_tracker.all_words[left[l]] < text_tracker.all_words[right[r]]:
+                t.append(right[r])
+                r += 1
+            else:
+                if(left[l] <= right[r]):
+                    t.append(left[l])
+                    l += 1
+                else:
+                    t.append(right[r])
+                    r += 1
+
+        while l < len(left):
+            t.append(left[l])
+            l += 1
+
+        while r < len(right):
+            t.append(right[r])
+            r += 1
+        
+        return t
+
+    def printtopfifty(array):
+        print("---Top 50---")
+
+        arr = text_tracker.SortFrequencies(array)
+        print(f"after sorted. ", arr)
+
+        for t in range(50):
+            if t >= len(arr):
+                break
+            print(f'{t}) {arr[t]}: {text_tracker.all_words[arr[t]]}')
+
 
 class dup_url:
     setOfURLs = set()
@@ -60,9 +112,11 @@ def extract_next_links(url, resp):
         #update the current frequency totals amongst all pages. Track the longest page.
         if(len(tokens) > text_tracker.longest_page[1]):
             text_tracker.longest_page = (url, len(tokens))
+            
         for t in frequencies:
             if t not in text_tracker.all_words:
                 text_tracker.all_words[t] = frequencies[t]
+                text_tracker.tokens.append(t)
             else:
                 text_tracker.all_words[t] += frequencies[t]
                     
